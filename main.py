e6ea8d2e9f61bc11237426563279ced7e1263438
Jeffry Samuel Eduarte Rojas <69224322+Jeffser@users.noreply.github.com>
2023-03-18 18:52:46 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import discord, json
+from discord.ext import tasks, commands
+from os import system
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
+author = False
+
+###################
+serverID = 0
+userID = 0
+botToken = ''
+###################
+
+@tasks.loop(minutes=0.1)
+async def record():
+    global author
+    if author == False: return
+    currentActivities = []
+    for cA in author.activities: currentActivities.append({'details': cA.details, 'large_image_url': cA.large_image_url, 'name': cA.name, 'state': cA.state})
+    currentActivities = json.dumps(currentActivities).replace('"', '\\"')
+    system(f'echo "{currentActivities}" > /srv/http/discord.json')
+    #with open("/srv/http/discord.json", "w") as outfile: outfile.write()
+
+@bot.event
+async def on_ready():
+    global author
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='you 🐱'))
+    author = bot.get_guild(serverID).get_member(userID)
+    record.start()
+
+if __name__ == "__main__":
+    bot.run(botToken)
